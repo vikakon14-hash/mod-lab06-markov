e6b6ea4e2c36533bb995015c20c939aeb26165e1
vikakon14-hash <vika.kon14@gmail.com>
2026-05-09 23:31:34 +0300

    Fix cpplint style errors 5

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,10 +1,9 @@
 // Copyright 2021 GHA Test Team
+#include <gtest/gtest.h>
 #include "textgen.h"
 
 #include <fstream>
 #include <string>
-
-#include <gtest/gtest.h>
 
 TEST(MarkovTest, test1) {
     statetab emptyTable;
